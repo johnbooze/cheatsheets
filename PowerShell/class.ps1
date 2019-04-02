cff22ec16594cd8cd2db88492ace6b5040b38ec9
John Booze <jbooze@microsoft.com>
2019-04-02 09:53:13 -0700

    Add class.ps1

---
+++ PowerShell/class.ps1
@@ -0,0 +1,24 @@
+﻿class Person
+{
+    [string] $name
+    hidden [ValidateRange(0,200)] [int] $age
+
+    Person([string] $name, [int] $age)
+    {
+        $this.name = $name
+        $this.age = $age
+    }
+
+    [void] HappyBirthday()
+    {
+        $this.age++
+    }
+
+    [string] Hello()
+    {
+        return "Hello $($this.name).  You are $($this.age) years old"
+    }
+}
+
+[Person] $jack = [Person]::new('Jack', 25)
+$jack.Hello()
