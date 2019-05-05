599808d5f3efa92995bc385753db63696e92172c
John Booze <jbooze@microsoft.com>
2019-05-05 11:15:44 -0700

    Add trick for tuples

--- PowerShell/tricks.ps1
+++ PowerShell/tricks.ps1
@@ -13,3 +13,12 @@ class MyObject
 $items = 1..3 | % { $str = 'x' * $_ ; [MyObject] @{ stringProperty = $str } }
 
 $items | select { $_.stringProperty.Length }
+
+# Tuples are an easy way to store multi-dimensional data
+$tuple1 = [System.Tuple]::Create('First Dimension','Second Dimension')
+$tuple2 = [System.Tuple]::Create('First Dimension','Second Dimension')
+$tuple1 -eq $tuple2
+$tupleHash = @{}
+$tupleHash.Add($tuple1, $tuple1)
+$tupleHash.Add($tuple2, $tuple2)
+# Error because the tuple uniquely defines the 'key'
